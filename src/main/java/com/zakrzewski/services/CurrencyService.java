e8f2cb2c016e9b5a48155d4e1c4abc3fa988c5ec
Michał Zakrzewski <michal.zakrzewsky@gmail.com>
2020-12-16 15:37:44 +0100

    Change to configuration bean

--- src/main/java/com/zakrzewski/services/CurrencyService.java
+++ src/main/java/com/zakrzewski/services/CurrencyService.java
@@ -1,6 +1,8 @@
 package com.zakrzewski.services;
 
 import com.zakrzewski.dto.CurrencyResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +11,13 @@ import java.util.HashMap;
 
 @Service
 public class CurrencyService {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public CurrencyService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public CurrencyResponse getCurrencyRates(){
         RestTemplate restTemplate = new RestTemplate();
